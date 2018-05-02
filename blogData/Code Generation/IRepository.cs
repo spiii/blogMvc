d3f370f6b3083281ade51bba88d99ae85dcf0115
spiii <ghdc@gmx.de>
2018-05-02 21:10:23 +0200

    housekeeping

--- blogData/Code Generation/IRepository.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-public interface IRepository<T> where T : class
-{
-    IEnumerable<T> GetAll();
-    T GetById(int id);
-    void Add(T entity);
-    void Delete(T entity);
-}
--- blogData/Code Generation/IUnitofWork.cs
+++
@@ -1,7 +0,0 @@
-
-using System;
-
-public interface IUnitOfWork : IDisposable
-{
-    void Save();
-}
