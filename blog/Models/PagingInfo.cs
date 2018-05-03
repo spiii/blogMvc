37435c2f2b1d36ab4b1b3a6bc568329577f7d7fc
spiii <ghdc@gmx.de>
2018-05-03 19:27:13 +0200

    Models

---
+++ blog/Models/PagingInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace blog.Models
+{
+    public class PagingInfo
+    {
+        public int CurrentPage { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
---
+++ blog/Models/PostsListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using blog.Controllers;
+using blogBl;
+
+namespace blog.Models
+{
+    public class PostsListViewModel
+    {
+        public IEnumerable<Post> posts;
+
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
