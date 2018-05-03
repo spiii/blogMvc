36606de28491ad3d5347bc601d6f5f52c16b74e5
spiii <ghdc@gmx.de>
2018-05-03 21:30:18 +0200

    mein Helper and Test

---
+++ blogTests/helpers/mainHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using blog.helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blog.helpers.Tests
+{
+    [TestFixture()]
+    public class mainHelperTests
+    {
+        [TestCase(4,true,"D")]
+        [TestCase(4,false,"d")]
+        [TestCase(5,false,"e")]
+        public void Number2StringTest(int position,bool isCap,string result)
+        {
+            // Act
+            string letter=MainHelper.number2String(position, isCap);
+            // Assert
+            Assert.AreEqual(letter, result);
+           
+        }
+    }
+}
