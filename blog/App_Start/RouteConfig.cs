fca6e0292f05254dffaa978ef2a5aefbcf2b0770
spiii <ghdc@gmx.de>
2018-05-03 20:14:36 +0200

    List Route

--- blog/App_Start/RouteConfig.cs
+++ blog/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace blog
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { Controller = "Post", action = "List" }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
