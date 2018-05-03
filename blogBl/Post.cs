150d68da9eaf1fce6b7b696d8806cedc4f4bc5f4
spiii <ghdc@gmx.de>
2018-05-03 19:27:25 +0200

    Bi changes

---
+++ blogBl/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blogBl
+{
+    public class Post
+    {
+        public int idPost { get; set; }
+        public string title { get; set; }
+
+    }
+}
