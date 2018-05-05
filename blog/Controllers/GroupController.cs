2b9c348cb837b0cf10f57bfb9b0e0e61e78dafe1
spiii <ghdc@gmx.de>
2018-05-05 20:04:26 +0200

    Logic group

---
+++ blog/Controllers/GroupController.cs
@@ -0,0 +1,30 @@
+﻿using blogBl.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace blog.Controllers
+{
+    public class GroupController : Controller
+    {
+        private IPostRepository repository;
+
+        public GroupController(IPostRepository postRepository)
+        {
+            this.repository = postRepository;
+        }
+
+        // GET: Group
+        public PartialViewResult Menue()
+        {
+            IEnumerable<string> groups = this.repository.posts
+                .SelectMany(x => x.groups.Select(y => y.groupName)
+                .Distinct()
+                .OrderBy(y => y));
+
+            return PartialView(groups);
+        }
+    }
+}
