296d68d9141f80041b3ff268989eba5a4a7e7e5a
spiii <ghdc@gmx.de>
2018-05-06 17:11:34 +0200

    add models vote and voteLine

---
+++ blogBl/VoteLine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace blogBl
+{
+    public class VoteLine
+    {
+        public Post post { get; set; }
+        public DateTime timestamp { get; set; }
+        public int quantity { get; set; }
+        public int userId { get; set; }
+        public bool upVote { get; set; }
+    }
+}
