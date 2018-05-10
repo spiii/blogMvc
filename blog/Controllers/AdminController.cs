a627e737939cfe9ac0861e9ec7323fc0c786caf8
spiii <ghdc@gmx.de>
2018-05-10 16:03:29 +0200

    admin controller with tests

---
+++ blog/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using blogBl.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace blog.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IPostRepository repository;
+
+        public AdminController(IPostRepository repository)
+        {
+            this.repository= repository;
+        }
+
+
+        public ViewResult Index()
+        {
+            return View(repository.posts);
+        }
+    }
+}
---
+++ blogTests/Controllers/AdminControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using blog.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using blogBl;
+using Moq;
+using blogBl.Abstract;
+using blogTests;
+
+namespace blog.Controllers.Tests
+{
+    [TestFixture()]
+    public class AdminControllerTests
+    {
+        [Test()]
+        public void IndexTest()
+        {
+            // Arrange
+            Mock<IPostRepository> mock = TestHelper.createMockObject();
+            AdminController target = new AdminController(mock.Object);
+            // Action
+            Post[] result = ((IEnumerable<Post>)target.Index().ViewData.Model).ToArray();
+            // Assert
+            Assert.AreEqual(result.Length, 5);
+            Assert.AreEqual("P1", result[0].title);
+            Assert.AreEqual("P2", result[1].title);
+            Assert.AreEqual("P3", result[2].title);
+            Assert.AreEqual("P4", result[3].title);
+            Assert.AreEqual("P5", result[4].title);
+        }
+    }
+}
